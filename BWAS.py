b8ca03bfbe0d7e87620ed96785b0d170f6fa2ead
kfirs127 <62557231+kfirs127@users.noreply.github.com>
2024-06-12 19:36:11 +0300

    initial update

---
+++ BWAS.py
@@ -0,0 +1,72 @@
+import heapq
+import numpy as np
+
+
+class Node:
+    def __init__(self, s, g, p, f):
+        self.s = s
+        self.g = g
+        self.p = None if p is None else Node(p.s, p.g, p.p, p.f)
+        self.f = f
+
+    def __lt__(self, other):
+        return self.f < other.f
+
+
+def BWAS(start, W, B, heuristic_function, T):
+    OPEN = []
+    CLOSED = {start: 0}
+    UB, n_UB = np.inf, None
+    LB = 0
+    expentions = 0
+    g = 0
+    p = None
+    f = heuristic_function([start])[0]
+    N_start = Node(start, g, p, f)
+    heapq.heappush(OPEN, N_start)
+
+    while len(OPEN) > 0 and expentions <= T:
+        generated = []
+        batch_expanstions = 0
+        while len(OPEN) > 0 and batch_expanstions < B and expentions <= T:
+            n = heapq.heappop(OPEN)
+            s, g, p, f = n.s, n.g, n.p, n.f
+            expentions += 1
+            batch_expanstions += 1
+            if len(generated) == 0:
+                LB = max(f, LB)
+            if s.is_goal():
+                if UB > g:
+                    UB, n_UB = g, n
+                continue
+            for successor in s.get_neighbors():
+                g_successor = g + 1  # c(s, s_) = 1
+                if successor not in CLOSED or g_successor < CLOSED[successor]:
+                    CLOSED[successor] = g_successor
+                    generated.append((successor, g_successor, n))
+        if LB >= UB:
+            return path_to_goal(n_UB), expentions
+        generated_states = [state for state, _, _ in generated]
+        heuristics = heuristic_function(generated_states)
+        for i in range(len(generated)):
+            s, g, p = generated[i]
+            h = heuristics[i]
+            f = g + W * h
+            n_successor = Node(s, g, p, f)
+            heapq.heappush(OPEN, n_successor)
+
+    return path_to_goal(n_UB), expentions
+
+
+def path_to_goal(n_UB):
+    if n_UB is None:
+        return None
+
+    path = []
+    current_s, current_p = n_UB.s, n_UB.p
+    while current_p is not None:
+        path.append(current_s)
+        current_s = current_p.s
+        current_p = current_p.p
+    path.append(current_s)
+    return path
---
+++ topspin.py
@@ -0,0 +1,49 @@
+from collections import deque
+import random
+
+
+class TopSpinState:
+
+    def __init__(self, state, k=4):
+        self._n = len(state)
+        self._k = k
+        self._state = deque(state)
+
+    def is_goal(self):
+        return list(self._state) == list(range(1, self._n + 1))
+
+    def get_state_as_list(self):
+        return list(self._state)
+
+    # we decided to take a random permutation of the possibale actions.
+    def get_neighbors(self):
+        lst = [self._flip_rotation(), self._counterclockwise_rotation(), self._clockwise_rotation()]
+        neighbors = lst[:]
+        random.shuffle(neighbors)
+        return neighbors
+
+    def _clockwise_rotation(self):
+        copy_state = self._state.copy()
+        copy_state.rotate(1)
+        return TopSpinState(list(copy_state))
+
+    def _counterclockwise_rotation(self):
+        copy_state = self._state.copy()
+        copy_state.rotate(-1)
+        return TopSpinState(list(copy_state))
+
+    def _flip_rotation(self):
+        copy_state = self._state.copy()
+        flip_rotation = TopSpinState(list(copy_state)[:self._k][::-1] + list(copy_state)[self._k:])
+        return flip_rotation
+
+    def __repr__(self):
+        return f"TopSpinState({list(self._state)})"
+
+    def __eq__(self, other):
+        if isinstance(other, TopSpinState):
+            return self._state == other._state
+        return False
+
+    def __hash__(self):
+        return hash(tuple(self._state))
